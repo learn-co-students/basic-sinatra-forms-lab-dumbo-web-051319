4a64605ad7f197b4216b7cab8d7f3ec2b52cf0de
github username <github email address>
2019-06-04 15:58:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
+require 'pry'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
-
+  post '/newteam' do
+    # binding.pry
+    erb :team
+  end
 end
