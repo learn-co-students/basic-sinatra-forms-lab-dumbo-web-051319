919e254e7057b096578ab2f105e75e66a7fdbc0f
SimplyAvi <a.totaram@gmail.com>
2019-06-05 15:59:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+erb:newteam
+end
 
+post '/team' do
+# binding.pry
+@name = params[:name]
+@coach = params[:coach]
+@point_guard = params[:pg]
+@Shooting_guard = params[:sg]
+@Power_forward = params[:pf]
+@Small_forward = params[:sf]
+@Center = params[:c]
+erb :team
+end
 
 end
